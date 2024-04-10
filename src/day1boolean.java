213f6bf217789f669c96eec72c0feed9d80d2aed
i.gimalov <i.gimalov@bftcom.com>
2024-04-11 00:04:30 +0500

    dz1

--- src/day1boolean.java
+++ src/day1boolean.java
@@ -1,2 +1,10 @@
 public class day1boolean {
+    public static void main(String[] args) {
+        int value = 0;
+        while(value < 5){
+            System.out.println("Hello");
+            value = value+1;
+        }
+
+    }
 }
--- src/program.java
+++ src/program.java
@@ -1,6 +1,12 @@
 public class program {
 
     public static void main (String[] args) {
-        System.out.println("Hello METANIT.com!");
+        int words = 0;
+        while (words<10){
+            System.out.print("Java"+" ");
+            words=words+1;
+        }
+
+
     }
 }
